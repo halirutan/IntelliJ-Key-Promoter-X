0af447e6541f354cf4ce0489c3acf154652fa77e
Patrick Scheibe <patrick@halirutan.de>
2020-12-20 04:25:21 +0100

    Set Java compatibility back to 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,8 +82,8 @@ tasks {
 
     withType<JavaCompile> {
         options.encoding = "UTF-8"
-        sourceCompatibility = "1.8"
-        targetCompatibility = "1.8"
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
     }
 
     withType<PatchPluginXmlTask> {
